6f41ce2d483734e5c3f63441fc0a95649de1d2c7
afterburner <rockey1024@icloud.com>
2018-12-10 15:50:09 +0800

    Finish cleanup

--- build/CMakeFiles/main.dir/DependInfo.cmake
+++ build/CMakeFiles/main.dir/DependInfo.cmake
@@ -4,14 +4,9 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/afterburner/CLionProjects/world/main.cpp" "/home/afterburner/CLionProjects/world/build/CMakeFiles/main.dir/main.cpp.o"
+  "/home/afterburner/CLionProjects/remove_periodic_noise/main.cpp" "/home/afterburner/CLionProjects/remove_periodic_noise/build/CMakeFiles/main.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
-
-# Preprocessor definitions for this target.
-set(CMAKE_TARGET_DEFINITIONS_CXX
-  "DEBUG"
-  )
 
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
-#include <fmt/format.h>
-#include <opencv2/opencv.hpp>
-#include <tuple>
+#include <fmt/core.h>                    // for print
+#include <opencv2/core/hal/interface.h>  // for CV_8U, CV_32FC1
+#include <opencv2/core.hpp>              // for normalize, merge, split, dft
+#include <opencv2/highgui.hpp>           // for imshow, waitKey, namedWindow
+#include <opencv2/imgproc.hpp>           // for circle
 
 namespace db {
-void imshow(const cv::String &winName_, cv::Mat img_) {
+void imshow(const cv::String &winName_, const cv::Mat& img_) {
 #ifdef DEBUG
   if (img_.depth() == CV_8U) {
     cv::imshow(winName_, img_);
@@ -30,11 +32,19 @@ void imshow(const cv::String &winName_, cv::Mat img_) {
 
 
 static std::tuple<cv::Mat, cv::Mat> calcPSD(const cv::Mat &input_image);
-static cv::Mat remove_periodic_noise(cv::Mat &input_image, const cv::Point2i noise_spike);
+static cv::Mat remove_periodic_noise(cv::Mat &&complex_, const cv::Point2i &noise_spike);
 
-int main() {
+int main(const int argc, const char* argv[]) {
+  if (argc < 2) {
+    fmt::print("Required a path to an image");
+    return 0;
+  }
   cv::Mat input_image = cv::imread(
-      "/home/afterburner/Pictures/periodic_noise.jpg", cv::IMREAD_GRAYSCALE);
+      argv[1], cv::IMREAD_GRAYSCALE);
+  if (input_image.empty()) {
+    fmt::print("Error reading image: {}", argv[1]);
+    return 1;
+  }
   cv::imshow("input_image", input_image);
   cv::Rect roi(0, 0, input_image.cols & -2, input_image.rows & -2);
   input_image = input_image(roi);
@@ -53,11 +63,13 @@ int main() {
     }
     },
           &noise_spike);
-  db::imshow(winName, magnitude);
+  cv::normalize(magnitude, magnitude, 0, 255, cv::NORM_MINMAX, CV_8U, {});
+  cv::imshow("magnitude", magnitude);
   cv::waitKey(0);
 
-  cv::Mat recovered_image = remove_periodic_noise(complex, noise_spike);
-  db::imshow("recovered image", recovered_image);
+  cv::Mat recovered_image = remove_periodic_noise(std::move(complex), noise_spike);
+  cv::normalize(recovered_image, recovered_image, 0, 255, cv::NORM_MINMAX, CV_8U, {});
+  cv::imshow("recovered image", recovered_image);
   cv::waitKey(0);
   return 0;
 }
@@ -73,29 +85,29 @@ std::tuple<cv::Mat, cv::Mat> calcPSD(const cv::Mat &input_image) {
   cv::Mat magnitude;
   cv::magnitude(channels[0], channels[1], magnitude);
   cv::pow(magnitude, 2, magnitude);
-  magnitude.at<float>(0) = 0;
+  magnitude.at<float>(0) = 0; //NOTE: very important, value at (0,0) is the main frequency of the whole image. For visualization reason, it must set to zero, otherwise it will dwarf all the noise spikes.
   return {magnitude, complex};
 }
 
-cv::Mat remove_periodic_noise(cv::Mat &input_image, const cv::Point2i noise_spike) {
+cv::Mat remove_periodic_noise(cv::Mat &&complex_, const cv::Point2i &noise_spike) {
   cv::Point2i c2, c3, c4;
   c2.x = noise_spike.x;
-  c2.y = input_image.rows - noise_spike.y;
-  c3.x = input_image.cols - noise_spike.x;
+  c2.y = complex_.rows - noise_spike.y;
+  c3.x = complex_.cols - noise_spike.x;
   c3.y = noise_spike.y;
-  c4.x = input_image.cols - noise_spike.x;
-  c4.y = input_image.rows - noise_spike.y;
-  cv::Mat anti_spike = cv::Mat::ones(input_image.size(), CV_32FC1);
+  c4.x = complex_.cols - noise_spike.x;
+  c4.y = complex_.rows - noise_spike.y;
+  cv::Mat anti_spike = cv::Mat::ones(complex_.size(), CV_32FC1);
   for (const auto &pt : {noise_spike, c2, c3, c4}) {
     cv::circle(anti_spike, pt, 10, {0}, -1);
   }
-  std::vector<cv::Mat> channles{
+  std::vector<cv::Mat> channels{
       std::move(anti_spike),
-      cv::Mat::zeros(input_image.size(), CV_32FC1)};
+      cv::Mat::zeros(complex_.size(), CV_32FC1)};
   cv::Mat anti_spike_composite;
-  cv::merge(channles, anti_spike_composite);
-  cv::mulSpectrums(input_image, anti_spike_composite, input_image, cv::DFT_ROWS);
-  cv::idft(input_image, input_image);
-  cv::split(input_image, channles);
-  return channles[0];
+  cv::merge(channels, anti_spike_composite);
+  cv::mulSpectrums(complex_, anti_spike_composite, complex_, cv::DFT_ROWS);
+  cv::idft(complex_, complex_);
+  cv::split(complex_, channels);
+  return channels[0];
 }
